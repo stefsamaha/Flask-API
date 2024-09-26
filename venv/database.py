a483f59143a2d45b1ba22f356aa120ca70e69731
Stefanie Samaha <sms111@mail.aub.edu>
2024-09-26 17:53:51 +0300

    Update project

--- venv/database.py
+++ venv/database.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # Optional for handling cross-origin requests
 import sqlite3
 
 def connect_to_db():
@@ -27,9 +25,6 @@ def create_db_table():
         print(f"User table creation failed - {e}")  # This will print the exact error message
     finally:
         conn.close()
-
-if __name__ == "__main__":
-    create_db_table()
 
 def insert_user(user):
     inserted_user = {}
@@ -128,41 +123,7 @@ def delete_user(user_id):
         conn.close()
     return message
 
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
-# Root route to handle requests to "/"
-@app.route('/')
-def home():
-    return "Welcome to the Flask API!"
-
-@app.route('/api/users', methods=['GET'])
-def api_get_users():
-    return jsonify(get_users())
-
-@app.route('/api/users/<user_id>', methods=['GET'])
-def api_get_user(user_id):
-    return jsonify(get_user_by_id(user_id))
-
-@app.route('/api/users/add', methods=['POST'])
-def api_add_user():
-    try:
-        user = request.get_json()
-        if not all(key in user for key in ("name", "email", "phone", "address", "country")):
-            return jsonify({"error": "Missing required fields"}), 400
-        return jsonify(insert_user(user))
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route('/api/users/update', methods=['PUT'])
-def api_update_user():
-    user = request.get_json()
-    return jsonify(update_user(user))
-
-@app.route('/api/users/delete/<int:user_id>', methods=['DELETE'])
-def api_delete_user(user_id):
-    return jsonify(delete_user(user_id))
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_db_table()
+
+
